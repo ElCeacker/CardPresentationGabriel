57bf288f5078aa6bb4bc1e0a9882782305a324ee
gabriel1010 <101879808+ElCeacker@users.noreply.github.com>
2022-10-05 10:50:34 +0100

    headBoard finiched

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.head}>
+        <View style={styles.imageBox}>
+          <Image source={require('./assets/imagenCurriculum.png')} style={styles.imagen}/>
+        </View>
+        <View style={styles.textBox}>
+          <Text style={styles.headText}>
+            Gabriel Alejandro Depool Martínez
+          </Text>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  head: {
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 2,
+    backgroundColor: 'grey',
+    paddingLeft: 30,
+  },
+  
+  imageBox: {
+    flexDirection: 'row',
+    width: '25%',
+    justifyContent: 'space-evenly',
+    marginLeft: 50
+  },
+  
+  imagen: {
+    width: 75, 
+    height: 75, 
+    borderRadius: 100,
+    textAlign: 'center',
+    paddingLeft: 30,
+  },
+  
+  textBox: {
+    flexDirection: 'row',
+    width: '90%'
+  },
+  
+  headText: {
+    fontSize: 30,
+    color: 'white',
+    alignItems: 'flex-end ', 
+    paddingRight: 12,
+    paddingLeft: 10
   },
 });
